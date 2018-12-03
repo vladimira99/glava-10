010f47dd1c860eb06c84e17af7d4f01048bed556
Vladimira Kamenova <44730166+vladimira99@users.noreply.github.com>
2018-12-03 11:57:59 +0200

    Add files via upload

---
+++ 10/10.1/10.1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            void Loops(int[] arr, int index)
+            {
+                if (index >= arr.Length)
+                {
+                    foreach (int element in arr) Console.Write("{0} ", element);
+                    Console.WriteLine();
+                }
+                else
+                    for (int i = 1; i <= arr.Length; i++)
+                    {
+                        arr[index] = i;
+                        Loops(arr, index + 1);
+                    }
+            }
+
+            void Main()
+            {
+                Console.Write("Enter N: ");
+                int n = Int32.Parse(Console.ReadLine());
+                int[] arr = new int[n];
+                Loops(arr, 0);
+            }
+        }
+    }
+}
---
+++ 10/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            double i = 1;
+            double a = 0;
+           
+            for (i=1; i<=49; i++)
+            {
+                Console.WriteLine("{i} root {i} = Math.Sqrt(i,1/i)} ");
+                a = a + Math.Sqrt(i, 1 / i);
+            }
+            Console.WriteLine(a);
+        }
+    }
+}
